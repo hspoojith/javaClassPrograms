8bb3b3eedfd49f77d4b11fae1f51fb0a5990f9a0
hspoojith <115713080+hspoojith@users.noreply.github.com>
2022-11-23 23:43:24 +0530

    Update assignemntOne.java

--- assignemntOne.java
+++ assignemntOne.java
@@ -7,19 +7,34 @@ class Student{
     int usn;
     char sem;
     int marks[] = new int[8];
-    <TO DO:Add Constructor to initialize sem and usn>
-    <TO DO : Add constructor with no intializations>
+
+    public Student(int usn, int sem) {
+
+    }
+
+    void display() {
+        System.out.println("USN :" + usn);
+        System.out.println("Semester :" + sem);//<TO DO:Add Constructor to initialize sem and usn>
+        System.out.println("Maximum marks:"+);
+    }
+
+    //<TO DO : Add constructor with no intializations>
     public void setSem() {
+        System.out.println("Enter Sem of student ");
         Scanner sc  = new Scanner(System.in);
         this.sem=sc.next().charAt(0);
     }
     public void setUsn(int usn) {
+        System.out.println("Enter usn of student");
         Scanner sc  = new Scanner(System.in);
         this.usn=sc.nextInt();
 
     }
 
-<TO DO : ADD METHOD TO SET MARKS>
+    public void setMarks(int[] marks) {
+        this.marks = marks;
+    }
+//<TO DO : ADD METHOD TO SET MARKS>
 
     public int[] getMarks() {
         return marks;
@@ -32,25 +47,41 @@ class Student{
     public int getUsn() {
         return usn;
     }
-    <TO DO: Add a method to find maximum marks>
-
-}
-
-
-public class assignemntOne {
-    public static void main(String[] args) {
-    Student eeeStudent1 = new Student();
-    int usn;
-    int sem;
-    Student eeeStudent2 = new Student(usn,sem);
-    <TO DO: Assign marks to students>
-
-    <TO DO: Print the max marks of both students>
-
-    <TO DO: Take input of a subject index from user and print
-    which student has more marks in that subject>
-
+    public int getMaximumMark() {
+        int max = Integer.MIN_VALUE;
+        for(int mark:marks) {
+            if(mark > max) {
+                max = mark;
+            }
+        }
+        return max; //<TO DO: Add a method to find maximum marks>
 
     }
-}
+
+
+    public class assignemntOne {
+        public static void main(String[] args) {
+            Student eeeStudent1 = new Student();
+            int usn;
+            int sem;
+            Student eeeStudent2 = new Student(usn, sem);
+            eeeStudent1.setMarks(new int[]{39, 43, 94, 60, 90, 56, 43, 32,});
+            eeeStudent2.setMarks(new int[]{50, 68, 78, 89, 57, 67, 87, 89}); //<TO DO: Assign marks to students>
+
+            eeeStudent1.setSem();
+            eeeStudent1.setUsn();
+            eeeStudent1.getMaximumMark();
+            eeeStudent1.display();
+
+            eeeStudent2.setSem();
+            eeeStudent2.setUsn();
+            eeeStudent2.getMaximumMark();
+            eeeStudent2.display();//<TO DO: Print the max marks of both students>
+
+            /*<TO DO: Take input of a subject index from user and print
+            which student has more marks in that subject>*/
+
+
+        }
+    }
 
